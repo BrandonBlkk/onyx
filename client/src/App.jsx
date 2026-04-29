8f935ba046ae0ef48759efb8351898993a6bc3dd
Kyaw Zayar Tun <kyawzayartun0527@gmail.com>
2026-04-30 00:24:12 +0630

    feat: add 404 page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Landing from './pages/Landing'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   )
