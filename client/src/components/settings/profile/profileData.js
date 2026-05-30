40ab7b5a6df51c4354af74e6d2d4678729fe8296
Kyaw Zayar Tun <kyawzayartun0527@gmail.com>
2026-05-30 23:22:48 +0100

    refactor: remove username input from profile page

--- client/src/components/settings/profile/profileData.js
+++ client/src/components/settings/profile/profileData.js
@@ -1,19 +1,12 @@
 export const profileFields = [
   {
     label: 'Name',
-    name: 'name',
-    defaultValue: 'Brandon',
-  },
-  {
-    label: 'Username',
-    name: 'username',
-    defaultValue: 'kyawzayartun0527',
+    name: 'name'
   },
   {
     label: 'Email Address',
     name: 'email',
     type: 'email',
-    defaultValue: 'kyawzayartun0527@gmail.com',
     helperText: 'Verified',
   },
 ]
