11b32194f05ae7e633c900dc033b742b67f9f9f4
Kyaw Zayar Tun <kyawzayartun0527@gmail.com>
2026-07-12 21:50:56 +0100

    feat: add get and update preferences apis

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -7,10 +7,12 @@ const router = express.Router();
 
 router.get('/me', authMiddleware, userController.getMe)
 router.get('/', userController.getAllUsers)
+router.get('/preferences', authMiddleware, userController.getPreferences)
 router.get('/:id', userController.getSingleUser)
 router.post('/', authController.createUser)
 router.post('/login', authController.loginUser)
 router.post('/forget-password', authController.forgetPassword)
+router.put('/preferences', authMiddleware, userController.updatePreferences)
 router.put('/:id', authMiddleware, userController.updateUser)
 router.delete('/:id', authMiddleware, userController.deleteUser)
 
